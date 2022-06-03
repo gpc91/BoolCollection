2ebb1db57ec0cf4cce12eef99632379326a8394a
George <gpc91@users.noreply.github.com>
2022-06-03 03:31:54 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,28 +1,24 @@
-﻿using Gpc91;
+﻿using System.Runtime.InteropServices;
+using Gpc91;
 
 public class Program
 {
     public static void Main()
     {
-        bool isHappy = true;
-        bool isSad = false;
-        bool isFast = true;
-        bool isSlow = false;
-        bool isAlive = true;
-        bool isDead = false;
-        bool canDance = false;
-        bool canSwim = true;
-        bool hasRhythm = false;
-        bool hasArms = true;
-
-        var col = BoolCollection.Create(isHappy, isSad, isFast, isSlow, isAlive, isDead, canDance, canSwim, hasRhythm, hasArms);
-        
-        Console.WriteLine(col);
-
-        var enumerator = col.GetEnumerator();
-
-        Console.WriteLine(col[5]);
-        col[7] = true;
-        Console.WriteLine(col[5]);
+        MemoryUsage();
     }
+    
+    public static void MemoryUsage()
+    {
+        bool[] values = {true, false, false, true, false, true, true, true, false, true, false, false, true, false, true, true, false};
+
+        BoolCollection collection = new BoolCollection();
+        collection.Build(values);
+
+        Console.WriteLine(sizeof(bool) * values.Length);
+        Console.WriteLine(Marshal.SizeOf(collection.Collection));
+        
+        Console.WriteLine(collection);
+    }
+
 }
